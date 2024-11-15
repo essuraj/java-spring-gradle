fdb18aca0270925e398f4b581acf352d4adede34
Suraj Elapully <essuraj@live.in>
2024-11-15 14:00:41 +0530

    add configuration files for Config API and Monitoring API; refactor NmsApplication to ConfigApplication and create MonitoringApplication

--- modules/config/src/main/java/org/bel/ConfigApplication.java
+++ modules/config/src/main/java/org/bel/ConfigApplication.java
@@ -1,4 +1,5 @@
-package org.bel.nms;
+package org.bel;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NmsApplication.class, args);
+    }
 
 }
--- modules/config/src/main/java/org/bel/Main.java
+++
@@ -1,7 +0,0 @@
-package org.bel;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
