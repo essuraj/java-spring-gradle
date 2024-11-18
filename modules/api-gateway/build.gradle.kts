ac13385dcf2a548dd66323058ebe4313b8f3c06d
Suraj Elapully <essuraj@live.in>
2024-11-18 16:56:44 +0530

    refactor API Gateway configuration; update application.yml for reactive web application type; move Redis configuration to data section; restore and update service registry application

--- modules/api-gateway/build.gradle.kts
+++ modules/api-gateway/build.gradle.kts
@@ -8,7 +8,9 @@ version = "0.0.1-SNAPSHOT"
 
 
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
+//    implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
+    // https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-gateway
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.5")
 //    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 
 }
